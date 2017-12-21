fbf5166e648a0707fbf1fe248d6dd17d70e58690
Solonel <30692287+Solonel@users.noreply.github.com>
2017-12-21 01:06:58 +0100

    Page du backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ReceiptsComponent } from './backend/receipts/receipts.component';
+import { BooksComponent } from './backend/books/books.component';
+import { IngredientsComponent } from './backend/ingredients/ingredients.component';
+import { BookComponent } from './backend/book/book.component';
+import { ReceiptComponent } from './backend/receipt/receipt.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceiptsComponent,
+    BooksComponent,
+    IngredientsComponent,
+    BookComponent,
+    ReceiptComponent
   ],
   imports: [
     BrowserModule
