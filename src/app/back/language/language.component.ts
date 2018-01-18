096f298f6c48057a38f42a190ebafe7cd4c66766
Solonel <30692287+Solonel@users.noreply.github.com>
2018-01-19 00:34:18 +0100

    Langue

--- src/app/back/language/language.component.ts
+++ src/app/back/language/language.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { LanguageService } from '../../services/language.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'lsc-language',
@@ -6,10 +10,205 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./language.component.css']
 })
 export class LanguageComponent implements OnInit {
+  /**
+  * Permet de temporiser le html tant que le chargement de la langue n'est pas terminé
+  */
+  isLoading = true;
 
-  constructor() { }
+  /**
+   * Si on ajoute un nouveau langage, il faut attendre que son ajout soit prêt
+   */
+  isNewLanguageReady = false;
 
-  ngOnInit() {
+  /**
+   * Le formulaire Group, il est de la form du ce qu'on souhaite avoir en sortie vers la base de données
+   */
+  languageForm: FormGroup;
+
+  /**
+   * Les langues de la langue  implémenté
+   */
+  implementedLanguages = [];
+
+  /**
+ * Les langues de la langue non implémenté
+ */
+  notImplementedLanguages = [];
+
+  /**
+   *  Dictionnaire de langue de la langue
+   */
+  locales: { [key: string]: FormGroup };
+
+  /**
+   *  Savoir si le langue existe
+   */
+  languageExist: boolean
+
+  /**
+   * Constructeur 
+   * @param route La route utilisé pour accéder à l'enregistrement
+   * @param router Le router pour faire une redirection
+   * @param fb Le formbuilder pour construire le formulaire react
+   * @param languageService Le service language pour récupérer les informations de traduction
+   */
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+    private languageService: LanguageService) {
+    this.createForm();
+    // On initialise l objet 
+    this.locales = {}
+    this.languageExist = false;
   }
 
+  /**
+   * Créé le formulaire React vide, nécessaire pour l'iniatilisation
+   */
+  createForm() {
+    this.languageForm = this.fb.group({
+      id: null,
+      locale: null,
+      flag: null
+    });
+  }
+
+  /**
+   * Initialisation de la langue
+   */
+  ngOnInit() {
+    this.getLanguage();
+  }
+
+  /**
+   * Récupère un langage par son Id
+   */
+  getLanguage(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    // Si notre id est bien récupéré
+    console.log(id)
+    if (id) {
+      // On récupère l'id que si il est correct
+      this.languageService.getLanguage(id).finally(() => {
+        // On n'est plus en loading
+        this.isLoading = false;
+      }).subscribe(language => {
+        this.languageForm.patchValue({
+          id: language.id,
+          flag: language.flag
+        });
+        this.setLocaleGroup(language.locale);
+        this.languageExist = true;
+      });
+    } else {
+      this.languageService.getNotImplementedLanguages([]).finally(() => {
+        this.isLoading = false;
+      }).subscribe(notImplementedLanguages => {
+        this.notImplementedLanguages = notImplementedLanguages;
+        this.languageExist = false;
+      }, err => {
+        console.log(err)
+      });
+    }
+  }
+
+  /**
+   * Créé les FormGroup par langue
+   * @param locale Liste langue sur la langue
+   */
+  setLocaleGroup(locale) {
+    // On créé un tableau temporaire pour avoir les id des langues de la langue
+    let localeIds = [];
+
+    // On parcours chaque objet de langue de la langue
+    for (let i in locale) {
+      // On créé un objet temporaire pour créé le formgroup
+      let obj = {
+        label: locale[i].label
+      }
+      this.locales[i] = this.fb.group(obj);
+
+      localeIds.push(i);
+    }
+
+    // Récupère les langues nécessaires
+    this.getLanguages(localeIds);
+
+    // On set le formgroup créé sur le formulaire pour disposer des controles coté html
+    this.languageForm.setControl('locale', this.fb.group(this.locales));
+  }
+
+  /**
+   * Récupère les langues implémentées et non implémentées de l'ingredient
+   * @param localeIds Le tableau d'id de langue
+   */
+  getLanguages(localeIds) {
+    // On récupère les langues implémentées et non implémentées
+    this.languageService.getImplementedLanguages(localeIds).subscribe(implementedLanguages => {
+      this.implementedLanguages = implementedLanguages
+    }, err => {
+      console.log(err);
+    });
+
+    this.languageService.getNotImplementedLanguages(localeIds).subscribe(notImplementedLanguages => {
+      this.notImplementedLanguages = notImplementedLanguages
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  /**
+   * Prépare le formulaire pour la nouvelle langue
+   * @param newLanguageId Identifiant de la nouvelle langue
+   */
+  addNewLanguage(newLanguageId) {
+    let obj = {
+      label: null
+    }
+    this.locales[newLanguageId] = this.fb.group(obj);
+    this.languageForm.setControl('locale', this.fb.group(this.locales));
+    this.isNewLanguageReady = true;
+  }
+
+  /**
+   * Redirige vers la liste des ingredients
+   */
+  goToList(): void {
+    this.router.navigate([`languages`]);
+  }
+
+  /**
+   * Redirige vers la langue avec l'identifiant cible
+   * @param id id de la langue
+   */
+  goToLanguage(id): void {
+    this.router.navigate([`language/${id}`])
+  }
+
+  /**
+   * Met à jour la langue
+   */
+  update() {
+    this.languageService.updateLanguage(this.languageForm.value)
+      .subscribe(() => { });
+  }
+
+  /**
+   * Supprime la langue
+   */
+  delete() {
+    this.languageService.deleteLanguage(this.languageForm.value)
+      .subscribe(() => { this.goToList() });
+  }
+
+  /**
+   * Ajoute une nouvelle langue
+   */
+  add() {
+    this.languageService.addLanguage(this.languageForm.value)
+      .subscribe(language => {
+        this.goToLanguage(language.id);
+      });
+  }
 }
--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -6,6 +6,9 @@ import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable()
 export class LanguageService {
 
@@ -52,6 +55,7 @@ export class LanguageService {
     let defaultLanguage = 'fr-fr';
     return defaultLanguage;
   }
+
   //
   getImplementedLanguages(locales): Observable<LanguageLight[]> {
     let implementedLanguages = [];
@@ -83,6 +87,58 @@ export class LanguageService {
       }
     }(locales));
     return of(notImplementedLanguages);
+  }
+
+  /** GET Language by id. Return `undefined` when id not found */
+  getLanguageNo404<Data>(id: number): Observable<Language> {
+    const url = `${this.languagesUrl}/?id=${id}`;
+    return this.http.get<Language[]>(url)
+      .pipe(
+      map(languages => languages[0]), // returns a {0|1} element array
+      tap(h => {
+        const outcome = h ? `fetched` : `did not find`;
+        this.log(outcome);
+      }),
+      catchError(this.handleError<Language>(`getLanguage id=${id}`))
+      );
+  }
+
+  /** GET Language by id. Will 404 if id not found */
+  getLanguage(id: string): Observable<Language> {
+    const url = `${this.languagesUrl}/${id}`;
+    return this.http.get<Language>(url).pipe(
+      tap(_ => this.log(`fetched Language id=${id}`)),
+      catchError(this.handleError<Language>(`getLanguage id=${id}`))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new Language to the server */
+  addLanguage(language: Language): Observable<Language> {
+    return this.http.post<Language>(this.languagesUrl, language, httpOptions).pipe(
+      tap((language: Language) => this.log(`added Language w/ id=${language.id}`)),
+      catchError(this.handleError<Language>('addLanguage'))
+    );
+  }
+
+  /** DELETE: delete the Language from the server */
+  deleteLanguage(language: Language | string): Observable<Language> {
+    const id = typeof language === 'string' ? language : language.id;
+    const url = `${this.languagesUrl}/${id}`;
+
+    return this.http.delete<Language>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted Language id=${id}`)),
+      catchError(this.handleError<Language>('deleteLanguage'))
+    );
+  }
+
+  /** PUT: update the Language on the server */
+  updateLanguage(language: Language): Observable<any> {
+    return this.http.put(this.languagesUrl, language, httpOptions).pipe(
+      tap(_ => this.log(`updated Language id=${language.id}`)),
+      catchError(this.handleError<any>('updateLanguage'))
+    );
   }
 
   /**
